f53339e15752de05ef0ab09035ea54f6bd1ab45d
Michael <m.zaluckyj@gmail.com>
2022-11-21 17:01:54 -0500

    exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is { quotient} remainder {remainder}");
         }
     }
 }
